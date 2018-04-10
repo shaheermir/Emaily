4f9b023ba289c3f48b1f85bed59e1e61b24602a3
Shaheer Mir <shaheermir0@gmail.com>
2018-04-10 15:18:48 -0400

    Adding Stripe checkout.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import Payments from './Payments'
+
 class Header extends React.Component {
   renderContent() {
     switch (this.props.auth) {
@@ -11,9 +13,14 @@ class Header extends React.Component {
         return <a href="/auth/google">Login With Google</a>
       default:
         return (
-          <li>
-            <a href="/api/logout">Logout</a>
-          </li>
+          <React.Fragment>
+            <li>
+              <Payments />
+            </li>
+            <li>
+              <a href="/api/logout">Logout</a>
+            </li>
+          </React.Fragment>
         )
     }
   }
