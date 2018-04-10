6e02ce58bb776277f45060e70fb1af8d4857ec9f
Shaheer Mir <shaheermir0@gmail.com>
2018-04-10 12:24:39 -0400

    Setting up Stripe keys.

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   MONGODB_URI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   port: process.env.PORT
 }
