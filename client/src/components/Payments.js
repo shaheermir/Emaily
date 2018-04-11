96d076382110302e6c8b50bbd86a37c48cf742c3
Shaheer Mir <shaheermir0@gmail.com>
2018-04-11 14:47:08 -0400

    Adding handleToken reducer.

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import StripeCheckout from 'react-stripe-checkout'
+
+import * as actions from '../actions'
 
 class Payments extends React.Component {
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 Email Credits."
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     )
   }
 }
 
-export default Payments
+export default connect(null, actions)(Payments)
