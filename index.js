a9c7ab8175914df47e20e51bef11176866d6ff60
Shaheer Mir <shaheermir0@gmail.com>
2018-04-12 18:43:14 -0400

    Setting up heroku configs for client build.

--- index.js
+++ index.js
@@ -27,5 +27,14 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = keys.port
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
