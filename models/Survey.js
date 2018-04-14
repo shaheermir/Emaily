78ee81739b5f85da153c5579be501caf48a92477
Shaheer Mir <shaheermir0@gmail.com>
2018-04-14 18:21:13 -0400

    Setting up Recipient model and survey routes.

--- models/Survey.js
+++ models/Survey.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose')
+const RecipientSchema = require('./Recipient')
 
 const SurveySchema = new mongoose.Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String]
+  recipients: [RecipientSchema],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 },
+  _user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  dateSent: Date,
+  lastResponded: Date
 })
 
 mongoose.model('surveys', SurveySchema)
