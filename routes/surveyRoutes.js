47a9d5711a8f568f4c9fa8ab1e9af61f1e1be2c0
Shaheer Mir <shaheermir0@gmail.com>
2018-04-22 19:49:18 -0400

    Finished webhook call.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,14 +11,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyID/:choice', (req, res) => {
     res.send('Thanks for voting!')
   })
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyID/:choice')
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ url, email }) => {
         const match = p.test(new URL(url).pathname)
 
@@ -28,6 +28,21 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyID')
+      .each(({ surveyID, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyID,
+            recipients: {
+              $elemMatch: { email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec()
+      })
       .value()
 
     res.send({})
